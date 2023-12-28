d4867e8959984c285f480e293f4901ba5768ce04
Asish Mahapatra <asishmahapatra918@gmail.com>
2023-12-28 22:35:20 +0530

    Bug Fixes

--- app/src/main/java/com/example/jetmusic/App.kt
+++ app/src/main/java/com/example/jetmusic/App.kt
@@ -1,6 +1,7 @@
 package com.example.jetmusic
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -8,6 +9,7 @@ import timber.log.Timber
 class App: Application() {
     override fun onCreate() {
         super.onCreate()
+        FirebaseApp.initializeApp(this)
         Timber.plant(Timber.DebugTree())
     }
 }
